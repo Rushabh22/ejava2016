184f8806239d4c1021e198e3ce25606bb5796ccf
amarjeet298 <amarjeet2982@gmail.com>
2016-10-22 14:29:44 +0800

    Amar - Committing ca1

---
+++ ca1/src/java/week3/business/PeopleBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3.business;
+
+import javax.ejb.Stateless;
+import week3.model.People;
+
+@Stateless
+public class PeopleBean {
+    
+    public void addUser(People  people){
+        
+        
+    }
+    
+}
---
+++ ca1/src/java/week3/model/People.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3.model;
+
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "people")
+@Entity
+public class People implements Serializable{
+
+    @Id
+    private Long pid;
+   
+    private String name;
+    
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getPid() {
+        return pid;
+    }
+
+    public void setPid(Long pid) {
+        this.pid = pid;
+    }
+
+
+    
+    
+    
+}
