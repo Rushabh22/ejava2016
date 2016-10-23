8035af1a86bec46e91037566b16ed1351768e3ca
Rushabh22 <A0135914@u.nus.edu>
2016-10-24 04:18:54 +0800

    Added logic to display list of appointments to the user on "Add Appointments" page

--- ca1/src/java/week3/rest/AppointmentResource.java
+++ ca1/src/java/week3/rest/AppointmentResource.java
@@ -1,50 +1,60 @@
-
 package week3.rest;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
+import javax.json.JsonArrayBuilder;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.json.JsonArray;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import week3.business.PeopleBean;
-import week3.model.People;
-
 
 @RequestScoped
 @Path("/appointment")
 public class AppointmentResource {
-    
-    @EJB private PeopleBean peopleBean;
-    
-        @GET
-        @Path("{email}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response findAppointments(@PathParam("email") String email) {
-            
-		List<Object> list = peopleBean.findAppointments(email);
-                
-                System.out.println("list >>" + list.size());
-                
-//                if(list != null && list.size() > 0){
-//                    JsonArray json = Json.createArrayBuilder();
-                    
-                return (Response.status(Response.Status.OK)
-                 .build());    
-                }
-                
-                                
-            
+
+    @EJB
+    private PeopleBean peopleBean;
+
+    @GET
+    @Path("{email}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAppointments(@PathParam("email") String email) {
+
+        List<Object[]> list = peopleBean.findAppointments(email);
+        System.out.println("email >> " + email);
+        JsonArrayBuilder jsnArrBuilder = Json.createArrayBuilder();
+
+        list.stream().map((record) -> {
+            Integer appointmentId = (Integer) record[0];
+            Long dateTime = ((java.util.Date) record[1]).getTime();
+            String description = (String) record[2];
+            String personId = (String) record[3];
+            System.out.println("***************************************");
+            System.out.println("appointmentId >> " + appointmentId);
+            System.out.println("dateTime >> " + dateTime);
+            System.out.println("description >> " + description);
+            System.out.println("personId >> " + personId);
+            JsonObject jsonObj = Json.createObjectBuilder()
+                    .add("appointmentId", appointmentId)
+                    .add("dateTime", dateTime)
+                    .add("description", description)
+                    .add("personId", personId)
+                    .build();
+            return jsonObj;
+        }).forEachOrdered((jsonObj) -> {
+            jsnArrBuilder.add(jsonObj);
+        });
+        System.out.println("***************************************");
+        System.out.println("list size >> " + list.size());
+        JsonArray jsnArray = jsnArrBuilder.build();
+        System.out.println(jsnArray);
+        return (Response.ok(jsnArray)).build();
+    }
 }
