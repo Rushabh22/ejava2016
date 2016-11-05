9ae98e47136f0dcf4de3d3c66e1234e6eaecf497
Rushabh22 <A0135914@u.nus.edu>
2016-11-05 18:04:47 +0800

    User login changes

---
+++ ca2/src/java/week4/web/UserSession.java
@@ -0,0 +1,41 @@
+package week4.web;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.Principal;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SessionScoped
+@Named
+public class UserSession implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject private Principal user;
+
+	public String getUsername() {
+		return (user.getName());
+	}
+	public void setUserName(String n) { }
+
+	public void logout() {
+
+		ExternalContext ctx = FacesContext.getCurrentInstance().getExternalContext();
+		HttpServletRequest req = (HttpServletRequest)ctx.getRequest();
+		HttpServletResponse resp = (HttpServletResponse)ctx.getResponse();
+		try {
+		req.getRequestDispatcher("/logout")
+				.forward(req, resp);
+		} catch (IOException | ServletException ex) { ex.printStackTrace();}
+		//FacesContext.getCurrentInstance().
+
+	}
+	
+}
