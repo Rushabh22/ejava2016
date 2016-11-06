7f9e4c0ad7812211e6747f1d86522bf8ddd54a98
ankur.jain <ankur.jain@rakuten.com>
2016-11-06 17:38:51 +0800

    Adding the mysql tables.

---
+++ ca2/userTables.sql
@@ -0,0 +1,43 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  ankur.jain
+ * Created: 6 Nov, 2016
+ */
+
+
+
+
+
+drop tables users;
+create table users(id integer not null generated always as identity(start with 1,increment by 1),
+username varchar(255),
+password varchar(255),
+email varchar(255),
+primary key (id)
+);
+drop table user_groups;
+create table user_groups (
+        id integer not null generated always as identity(start with 1,increment by 1),
+	groupname varchar(255),
+	username varchar(255),
+	primary key (id)
+);
+select * from users;
+delete from users where id=1;
+insert into users(username,password,email) values ('super','73d1b1b1bc1dabfb97f216d897b7968e44b06457920f00f2dc6c1ed3be25ad4c','admin@my.com');
+insert into users(username,password,email) values ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','abc@my.com');
+insert into users(username,password,email) values ('ankur','4b81d9c157b819532e78eab4ee305e3947602d8d444874cb4a9199731ec61adc','ankur@my.com');
+
+insert into user_groups (groupname,username) values ('admingroup','super');
+
+insert into user_groups (groupname,username) values ('usergroup','super');
+insert into user_groups (groupname,username) values ('admingroup','admin');
+
+
+insert into user_groups (groupname,username) values ('usergroup','ankur');
+
+
