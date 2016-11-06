8000ba1f031f5d8df392a2289e785c5c444103ae
ankur.jain <ankur.jain@rakuten.com>
2016-11-06 17:28:39 +0800

    Adding the flow for registering a new user.

---
+++ ca2/src/java/week4/bean/NewUserBean.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week4.bean;
+
+import java.io.Serializable;
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author ankur.jain
+ */
+@Named
+@RequestScoped
+public class NewUserBean{
+  @Resource(name="jdbc/jdbcRealm")
+  private DataSource ds;
+  
+  private String username;
+  private String password;
+  private String email;
+  private String userType;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+  
+ 
+}
