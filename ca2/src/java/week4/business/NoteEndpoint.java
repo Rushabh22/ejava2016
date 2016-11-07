98072cc5212743e2f8fcc583a7d66f441052a027
Rushabh22 <A0135914@u.nus.edu>
2016-11-07 08:05:06 +0800

    Initial commit for Websocket implementation

---
+++ ca2/src/java/week4/business/NoteEndpoint.java
@@ -0,0 +1,42 @@
+package week4.business;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+@RequestScoped
+@ServerEndpoint("/note/{category}")
+public class NoteEndpoint {
+
+	private Session session;
+
+	@OnOpen
+	public void open(Session sess,EndpointConfig config,@PathParam("category") String category) {
+		session = sess;
+		System.out.println(">>> session id: " + sess.getId());
+	}
+
+	@OnMessage
+	public void message(String text) {
+		String msg = Json.createObjectBuilder()
+				.add("text", text)
+				.add("time", (new Date()).toString())
+				.build()
+				.toString();
+
+		for (Session s: session.getOpenSessions())
+			try {
+				s.getBasicRemote().sendText(msg);
+			} catch (IOException ex) {
+				try { s.close(); } catch (IOException e) {} 
+			}
+	}
+}
