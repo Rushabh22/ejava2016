0ca7230c3c5eadd1ffdc5143c45ee8e2240cada9
ankur.jain <ankur.jain@rakuten.com>
2016-11-07 15:02:23 +0800

    Adding some more jsf pages.

--- ca2/src/java/week4/bean/NewUserBean.java
+++ ca2/src/java/week4/bean/NewUserBean.java
@@ -1,61 +1,177 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package week4.bean;
 
-import javax.annotation.Resource;
-import javax.faces.bean.RequestScoped;
-import javax.inject.Named;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.sql.DataSource;
+import javax.annotation.Resource;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
-/**
- *
- * @author ankur.jain
- */
 @Named
 @RequestScoped
-public class NewUserBean{
-  @Resource(name="jdbc/jdbcRealm")
-  private DataSource ds;
-  
-  private String username;
-  private String password;
-  private String email;
-  private String userType;
+public class NewUserBean {
 
-    public String getUsername() {
-        return username;
-    }
+	@Resource(name = "jdbc/jdbcRealm")
+	private DataSource ds;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	@Size(min = 3, max = 50)
+	private String username;
 
-    public String getPassword() {
-        return password;
-    }
+	@Size(min = 3, max = 50)
+	private String password;
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	@Pattern(regexp
+			= "[\\w\\.-]*[a-zA-Z0-9_]@[\\w\\.-]*[a-zA-Z0-9]\\.[a-zA-Z][a-zA-Z\\.]*[a-zA-Z]")
+	private String email;
 
-    public String getEmail() {
-        return email;
-    }
+	private String usertype;
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public String getUsertype() {
+		return usertype;
+	}
 
-    public String getUserType() {
-        return userType;
-    }
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
 
-    public void setUserType(String userType) {
-        this.userType = userType;
-    }
-  
- 
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// SHA-256 hash code to encrypt the password
+	public String encryptPassword(String data) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(data.getBytes("UTF-8")); // Change this to "UTF-16" if needed
+			byte[] digest = md.digest();
+			BigInteger bigInt = new BigInteger(1, digest);
+			return bigInt.toString(16);
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+		}
+		return "encryption error";
+	}
+
+	public String add() {
+
+		FacesContext context = FacesContext.getCurrentInstance();
+
+		if (ds == null) {
+			return "db_connection_error";
+		}
+
+		Connection conn;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			return "db_connection_error";
+		}
+
+		// check if 'username' is already registered
+		int count = 0;
+		try {
+			PreparedStatement countQuery = conn.prepareStatement(
+					"SELECT COUNT(*) AS total from users WHERE username = ?");
+			countQuery.setString(1, username);
+			ResultSet rs = countQuery.executeQuery();
+			if (rs.next()) {
+				count = rs.getInt("total");
+			}
+		} catch (SQLException e) {
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				return "db_connection_error";
+			}
+		}
+		if (count != 0) {
+			context.addMessage("f:username",
+					new FacesMessage("Username already exists"));
+			return null;
+		}
+
+		// now insert a new user and usergroup
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			return "db_connection_error";
+		}
+
+		boolean committed = false;
+
+		try {
+
+			conn.setAutoCommit(false);
+
+			PreparedStatement insertUserQuery = conn.prepareStatement(
+					"INSERT INTO USERS (USERNAME, PASSWORD, EMAIL) VALUES (?, ?, ?)");
+			insertUserQuery.setString(1, username);
+			insertUserQuery.setString(2, encryptPassword(password));
+			insertUserQuery.setString(3, email);
+			insertUserQuery.executeUpdate();
+
+			PreparedStatement insertGroupQuery = conn.prepareStatement(
+					"INSERT INTO USER_GROUPS (GROUPNAME, USERNAME) VALUES (?, ?)");
+			if (usertype.equals("user") || usertype.equals("user+admin")) {
+				insertGroupQuery.setString(1, "user"); // group name
+				insertGroupQuery.setString(2, username);
+				insertGroupQuery.executeUpdate();
+			}
+			if (usertype.equals("admin") || usertype.equals("user+admin")) {
+				insertGroupQuery.setString(1, "admin"); // group name
+				insertGroupQuery.setString(2, username);
+				insertGroupQuery.executeUpdate();
+			}
+
+			conn.commit(); // both user and usergroup are inserted successfully
+			committed = true;
+			context.addMessage(null,
+					new FacesMessage(username + ": successfully inserted."));
+
+		} catch (SQLException e) {
+			context.addMessage(null,
+					new FacesMessage("SQLException to insert user/usergroup"));
+		} finally {
+			try {
+				if (!committed) {
+					conn.rollback();
+				}
+				conn.close();
+			} catch (SQLException e) {
+				return "db_connection_error";
+			}
+		}
+
+		return null;
+	}
 }
---
+++ ca2/src/java/week4/bean/User.java
@@ -0,0 +1,52 @@
+package week4.bean;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.security.Principal;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Named
+@SessionScoped
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+        private String name;
+        public User() {
+            ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+            HttpServletRequest request = (HttpServletRequest)context.getRequest();
+            name=request.getRemoteUser();
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	
+
+	public String logout() {
+                FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+                return "/index";
+	}
+        
+        public String createNote() {
+           return null; 
+        }
+        public String listNote() {
+            return null;
+        }
+
+	
+}
