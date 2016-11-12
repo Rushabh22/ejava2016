c18da6220fb8921727a952dabb07aa9d6829b3a1
amarjeet298 <amarjeet2982@gmail.com>
2016-11-12 17:29:01 +0800

    HQ update logic

--- ca3/src/java/week5/proxy/FileUploadServlet.java
+++ ca3/src/java/week5/proxy/FileUploadServlet.java
@@ -10,6 +10,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -18,42 +20,90 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+import org.glassfish.jersey.media.multipart.MultiPart;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import week5.entity.Pod;
 import week5.service.LogisticsService;
+import week5.util.AppConstants;
 
 @WebServlet("/upload")
 @MultipartConfig
-public class FileUploadServlet extends HttpServlet{
+public class FileUploadServlet extends HttpServlet {
 
     @EJB
     LogisticsService logisticsService;
-    
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         Integer podId = Integer.valueOf(request.getParameter("podId"));
         String note = request.getParameter("note");
-        Date time = new Date(request.getParameter("time"));
+        Long timeLong = Long.valueOf(request.getParameter("time"));
+        Date time = new Date(timeLong);
         byte[] image = readPart(request.getPart("image"));
-           Pod pod = new Pod();
-            pod.setNote(note);
-            pod.setPod_id(podId);
-            pod.setDelivery_date(time);
-            pod.setImage(image);
-            logisticsService.updatePod(pod);
+        Pod pod = logisticsService.findPod(podId);
+        pod.setNote(note);
+        pod.setPod_id(podId);
+        pod.setDelivery_date(time);
+        pod.setImage(image);
+        logisticsService.updatePod(pod);
+
+        //notify HQ
+        Client client = ClientBuilder.newBuilder()
+                .register(MultiPartFeature.class)
+                .build();
+
+        MultiPart part = new MultiPart();
+
+        // byte[] image = readImage(podId);
+        MultiPart formData = new FormDataMultiPart()
+                .field("teamId", AppConstants.TEAM_ID, MediaType.TEXT_PLAIN_TYPE)
+                .field("podId", podId, MediaType.TEXT_PLAIN_TYPE)
+                .field("callback", "http://10.10.3.186/ca3/callback", MediaType.TEXT_PLAIN_TYPE)
+                .field("note", note, MediaType.TEXT_PLAIN_TYPE)
+                .field("image", image, MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        formData.setMediaType(MediaType.MULTIPART_FORM_DATA_TYPE);
+
+        //part.bodyPart(new FileDataBodyPart("image", 
+        //new File("/home/cmlee/Pictures/ca3.png")));
+        Response callResp = null;
+        do {
+            WebTarget target = client.target("http://10.10.0.48:8080/epod/upload");
+            Invocation.Builder inv = target.request();
+            System.out.println("I am here, going to upload  now");
+            System.out.println(">>> part: " + formData);
+
+            callResp = inv.post(Entity.entity(formData, formData.getMediaType()));
+            System.out.println("After involing the Post >>> resp code >> " + callResp.getStatus());
+            try {
+                Thread.currentThread().sleep(25000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(FileUploadServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } while (callResp.getStatus() != 200);
+
     }
 
     private byte[] readPart(Part part) throws IOException {
-        byte[] buffer = new byte[1024*8];
+        byte[] buffer = new byte[1024 * 8];
         int size = 0;
-        try(InputStream is = part.getInputStream()){
+        try (InputStream is = part.getInputStream()) {
             BufferedInputStream bis = new BufferedInputStream(is);
-            try(ByteArrayOutputStream baos = new ByteArrayOutputStream()){
-                while (-1 != (size = bis.read(buffer)))
-                    baos.write(buffer,0,size);
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                while (-1 != (size = bis.read(buffer))) {
+                    baos.write(buffer, 0, size);
+                }
                 buffer = baos.toByteArray();
             }
         }
         return buffer;
     }
-    
+
 }
--- ca3/src/java/week5/service/LogisticsService.java
+++ ca3/src/java/week5/service/LogisticsService.java
@@ -42,4 +42,6 @@ public class LogisticsService {
   public Pod findPod(Integer id){
       return em.find(Pod.class, id);
   }
+
+
 }
