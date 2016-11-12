14bfc870d6a9217d1d963b5670b878e58de3b821
ankur.jain <ankur.jain@rakuten.com>
2016-11-12 11:16:06 +0800

    Adding JSF Pages and the Service Layer Business Logic.

--- ca3/src/java/week5/service/LogisticsService.java
+++ ca3/src/java/week5/service/LogisticsService.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package week5.service;
 
-/**
- *
- * @author ankur.jain
- */
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import week5.entity.Delivery;
+
+
+@Stateless
 public class LogisticsService {
     
+    @PersistenceContext(unitName="ca3PU")
+    private EntityManager em;
+    
+    public void saveDelivery(Delivery note) {
+        em.persist(note);        
+    }
 }
